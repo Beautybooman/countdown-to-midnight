2780030b3a99e7905faad854c64ade556ec73352
Isaiah Davis <ID@Isaiahs-iMac.local>
2021-02-04 11:46:31 -0500

    commit tree c680370826409b7601bd157ed440156e87659bca

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
-
-def countdown
+# Loops
+def countdown(num)
+    while num > 0 # = 10
+        puts "#{num} SECOND(S)!"
+        num -= 1 # decrement 1 assigns ne num
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!" #implicit return
 end
+
+def countdown_with_sleep(num)
+    while num > 0 # = 10
+        puts "#{num} SECOND(S)!"
+        num -= 1 # decrement 1 assigns ne num
+        sleep(1)
+    end
+    "HAPPY NEW YEAR!" #implicit return
+end
+sleep(5)
